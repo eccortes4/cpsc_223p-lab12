48f7ac102224a5fd063051043f8b481ba5a770e7
Philip Sanchez <philipsanchez14@csu.fullerton.edu>
2025-05-01 13:11:00 -0700

    added _save_account function

--- user.py
+++ user.py
@@ -1,6 +1,6 @@
-
+import json
 class User:
-    user_list = {}
+    user_list = []
 
     def __init__(self, name, password):
         self.name = name
@@ -12,6 +12,9 @@ class User:
     def changePassword(self, new_password):
         self.__password = new_password
 
+    def _save_account(self) :
+        with open('account_management', 'w') as file:
+            json.dump(self.user_list, file)
 
     def login(self, name, password) :
         if self.name in self.user_list :
@@ -19,6 +22,10 @@ class User:
             if self.__password in self.user_list :
                 print("Correct Password")
                 print("Login Successful")
+                user = {
+                    name : password
+                }
+                self.user_list.append(user)
                 self.logedin = True
                 return True
             else :
